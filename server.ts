df792e6ece638a53112c4b1b2ce7b17e71a1af97
Abraham Jose <abrahamjose002@gmail.com>
2024-10-13 22:31:49 +0530

    database condition added

--- server.ts
+++ server.ts
@@ -2,21 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-
-
 import router from './src/routes/authRoute';
 import connectDB from './src/config/db';
 import imageRouter from './src/routes/imageRoute';
 
-
 dotenv.config();
-
 
 const app = express();
 
 
-connectDB();
-
+connectDB()
+    .then(() => console.log('Connected to Database'))
+    .catch(err => {
+        console.error('Database connection failed:', err);
+        process.exit(1); 
+    });
 
 app.use(express.json());
 app.use(cors({
@@ -29,8 +29,8 @@ app.use(cookieParser());
 app.use('/api/auth', router);
 app.use('/api/image', imageRouter);
 
-
 const port = process.env.PORT || 10000;
+
 app.listen(port, () => {
-    console.log(`Server running on PORT : ${port}`);
+    console.log(`Server running on PORT: ${port}`);
 });
