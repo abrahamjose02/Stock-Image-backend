9d631e09133d0c8110166d2b99382fec3589a98c
Abraham Jose <abrahamjose002@gmail.com>
2024-10-13 21:43:19 +0530

    files changed

--- server.ts
+++ server.ts
@@ -1,7 +1,36 @@
-import app from './app'
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+
+import router from './src/routes/authRoute';
+import connectDB from './src/config/db';
+import imageRouter from './src/routes/imageRoute';
+
+
+dotenv.config();
+
+
+const app = express();
+
+
+connectDB();
+
+
+app.use(express.json());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
+app.use(cookieParser());
+
+app.use('/api/auth', router);
+app.use('/api/image', imageRouter);
+
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT,()=>{
-    console.log(`Server running on PORT : ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on PORT : ${PORT}`);
 });
