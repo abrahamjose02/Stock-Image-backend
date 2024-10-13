b66de0396ac03d53e4af5b0ddac9c727c06d93d5
Abraham Jose <abrahamjose002@gmail.com>
2024-10-13 21:51:30 +0530

    port changed

--- server.ts
+++ server.ts
@@ -30,7 +30,7 @@ app.use('/api/auth', router);
 app.use('/api/image', imageRouter);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on PORT : ${PORT}`);
 });
